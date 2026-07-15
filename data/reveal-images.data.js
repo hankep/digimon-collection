b3c51f2d0cb922355ebc1bc5c96810b4147697a9
hankep <Phil.Hanke@dataciders.com>
2026-07-15 10:50:13 +0200

    reveal-images.data.js: erledigte Einträge aufgeräumt

--- data/reveal-images.data.js
+++ data/reveal-images.data.js
@@ -1,9 +1,5 @@
 window.REVEAL_IMAGES = {
   "BT26-025": "https://assets.cardlist.dev/images/communitycards/BT26-025_1f2f19en-US.png",
   "BT26-026": "https://assets.cardlist.dev/images/communitycards/BT26-026_c9eb53en-US.png",
-  "BT26-029": "https://assets.cardlist.dev/images/communitycards/BT26-029_19788fen-US.jpg",
-  "BT26-033": "https://assets.cardlist.dev/images/communitycards/BT26-033_d8957cen-US.png",
-  "BT26-062": "https://assets.cardlist.dev/images/communitycards/BT26-062_0ff9b1en-US.jpg",
-  "BT26-073": "https://assets.cardlist.dev/images/communitycards/BT26-029_ae559den-US.jpg",
   "BT26-090": "https://assets.cardlist.dev/images/communitycards/BT26-090_a41556en-US.jpg"
 };
