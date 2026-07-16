dc43411ec0b40b09105e9fbf99cc16016042006a
hankep <50618028+hankep@users.noreply.github.com>
2026-07-16 09:12:38 +0000

    Automatischer Karten-Sync (sync-cards.py)

--- data/reveal-images.data.js
+++ data/reveal-images.data.js
@@ -1,5 +1,8 @@
 window.REVEAL_IMAGES = {
+  "BT26-005": "https://assets.cardlist.dev/images/communitycards/BT26-005_1f55e0en-US.jpg",
   "BT26-025": "https://assets.cardlist.dev/images/communitycards/BT26-025_1f2f19en-US.png",
   "BT26-026": "https://assets.cardlist.dev/images/communitycards/BT26-026_c9eb53en-US.png",
-  "BT26-090": "https://assets.cardlist.dev/images/communitycards/BT26-090_a41556en-US.jpg"
+  "BT26-065": "https://assets.cardlist.dev/images/communitycards/BT26-065_3c0684en-US.jpg",
+  "BT26-090": "https://assets.cardlist.dev/images/communitycards/BT26-090_a41556en-US.jpg",
+  "BT26-101": "https://assets.cardlist.dev/images/communitycards/BT26-101_8332bfen-US.jpeg"
 };
