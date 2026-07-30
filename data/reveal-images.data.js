4dbb5b1e0b24490e8a340fe28093c170a61a8cd1
hankep <50618028+hankep@users.noreply.github.com>
2026-07-30 09:29:39 +0000

    Automatischer Karten-Sync (sync-cards.py)

--- data/reveal-images.data.js
+++ data/reveal-images.data.js
@@ -1,5 +1,1 @@
-window.REVEAL_IMAGES = {
-  "BT26-040": "data/reveal-cache/BT26-040.jpg",
-  "BT26-055": "data/reveal-cache/BT26-055.jpg",
-  "BT26-077": "data/reveal-cache/BT26-077.jpg"
-};
+window.REVEAL_IMAGES = {};
